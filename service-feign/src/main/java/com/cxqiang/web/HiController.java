8ad1a3f2e8702b2b8f42e959bc14ecb81898281b
xu <alvin.xuqiang@gmail.com>
2017-08-15 16:46:14 +0800

    spring cloud init

--- service-feign/src/main/java/com/cxqiang/web/HiController.java
+++ service-feign/src/main/java/com/cxqiang/web/HiController.java
@@ -2,22 +2,25 @@ package com.cxqiang.web;
 
 import com.cxqiang.service.SchedualServiceHi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by xuqiang
  * 2017/8/11.
  */
-@RestController
+@Controller
 public class HiController {
     @Autowired
     SchedualServiceHi schedualServiceHi;
 
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
-    public String sayHi(@RequestParam String name){
-        return schedualServiceHi.sayHiFromClientOne(name);
+    public String sayHi(@RequestParam String name,Model model){
+        String clientOne = schedualServiceHi.sayHiFromClientOne(name);
+        model.addAttribute("name",clientOne);
+        return "index";
     }
 }
