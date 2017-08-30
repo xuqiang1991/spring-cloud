76c9bccf476ab1bc1b9aadcfeb106cf09bca0978
xu <alvin.xuqiang@gmail.com>
2017-08-30 15:10:31 +0800

    spring cloud init

--- service-feign/src/main/java/com/cxqiang/config/WebSecurityConfiguration.java
+++ service-feign/src/main/java/com/cxqiang/config/WebSecurityConfiguration.java
@@ -2,16 +2,16 @@ package com.cxqiang.config;
 
 import com.cxqiang.entity.Account;
 import com.cxqiang.service.UserInfoService;
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -30,32 +30,27 @@ public class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdap
 
     @Override
     public void init(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService());
+        auth.userDetailsService(userDetailsService()).passwordEncoder(new Md5PasswordEncoder());
     }
 
     @Bean
     UserDetailsService userDetailsService() {
 
-        return new UserDetailsService() {
-
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                Account account = userInfoService.findByUsername(username);
-                if(account != null) {
-                    List<GrantedAuthority> authorities = null;
-                    if(CollectionUtils.isNotEmpty(account.getAuthoritys())){
-                        authorities = new ArrayList();
-                        for(String role : account.getAuthoritys()) {
-                            authorities.add(new SimpleGrantedAuthority(role));
-                        }
+        return username -> {
+            Account account = userInfoService.findByUsername(username);
+            if(account != null) {
+                List<GrantedAuthority> authorities = null;
+                if(CollectionUtils.isNotEmpty(account.getAuthoritys())){
+                    authorities = new ArrayList();
+                    for(String role : account.getAuthoritys()) {
+                        authorities.add(new SimpleGrantedAuthority(role));
                     }
-                    return new User(account.getUsername(), account.getPassword(), true, true, true, true,
-                            authorities);
-                } else {
-                    throw new UsernameNotFoundException("could not find the user '" + username + "'");
                 }
+                return new User(account.getUsername(), account.getPassword(), true, true, true, true,
+                        authorities);
+            } else {
+                throw new UsernameNotFoundException("could not find the user '" + username + "'");
             }
-
         };
     }
 }
