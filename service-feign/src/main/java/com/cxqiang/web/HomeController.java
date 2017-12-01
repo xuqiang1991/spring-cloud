2fa3373ada3696da2ad01588a5e180bd58d497a4
xu <alvin.xuqiang@gmail.com>
2017-12-01 15:29:23 +0800

    mybatis-generator-maven-plugin

--- service-feign/src/main/java/com/cxqiang/web/HomeController.java
+++ service-feign/src/main/java/com/cxqiang/web/HomeController.java
@@ -4,16 +4,13 @@ import com.cxqiang.entity.sys.SysMenu;
 import com.cxqiang.service.SysService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -42,13 +39,12 @@ public class HomeController {
     }
 
 
-    @RequestMapping(value = "/logout",method = RequestMethod.GET)
-    public String logout(HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-        return "redirect:/";
+    @RequestMapping(value = "/logout1111",method = RequestMethod.GET)
+    @ResponseBody
+    public String logout() {
+        SecurityContextHolder.clearContext();
+        SecurityContextHolder.getContext().setAuthentication(null);
+        return "redirect";
     }
 
     @RequestMapping(value = "/test",method = RequestMethod.GET)
